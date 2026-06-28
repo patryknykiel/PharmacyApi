480780b09826b61b4b8be381f55e4e55318cfed7
patryknykiel <pnykiel72@gmail.com>
2026-06-28 18:17:48 +0200

    P_U03/2

--- Pharmacy/Program.cs
+++ Pharmacy/Program.cs
@@ -6,9 +6,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
@@ -18,6 +27,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseRouting();
+
+app.UseCors();
+
+app.UseAuthorization();
+app.MapControllers();
 
 app.UseHttpsRedirection();
 
